543dd7a10f0a857e3f12d67d4ba1f58b20c01f8c
K0teize <ktv110687@gmail.com>
2025-06-26 13:33:36 +0600

    feat: add extension method realisation

---
+++ task01/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace task01;
+
+public static class StringExtension
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return false;
+        }
+        var cleanInput = new string(input.ToLower().Where(c => !char.IsWhiteSpace(c) && !char.IsPunctuation(c)).ToArray());
+        var revClean = new string(cleanInput.Reverse().ToArray());
+        return cleanInput == revClean;
+    }
+}
