2e9f2eac5f3872bf901c93845a7e133b5a340564
K0teize <ktv110687@gmail.com>
2025-07-01 18:14:12 +0600

    feat: add reflection project

---
+++ task05/Class1.cs
@@ -0,0 +1,39 @@
+﻿namespace task05;
+
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+public class ClassAnalyzer
+{
+    private Type _type;
+
+    public ClassAnalyzer(Type type)
+    {
+        _type = type;
+    }
+    public IEnumerable<string> GetPublicMethods()
+    {
+        var method = _type.GetMethods();
+        return method.Select(s => s.Name);
+    }
+    public IEnumerable<string> GetMethodParams(string methodname)
+    {
+        var method = _type?.GetMethod(methodname);
+        return method?.GetParameters().Select(s => s.Name!)??Enumerable.Empty<string>();
+    }
+    public IEnumerable<string> GetAllFields()
+    {
+        var fields = _type.GetFields(BindingFlags.NonPublic|BindingFlags.Public|BindingFlags.Instance);
+        return fields.Select(s => s.Name);
+    }
+    public IEnumerable<string> GetProperties()
+    {
+        var properties = _type.GetProperties();
+        return properties.Select(s => s.Name);
+    }
+    public bool HasAttribute<T>() where T : Attribute
+    {
+        return Attribute.IsDefined(_type,typeof(T));
+    }
+}
