2285629a62231634103d430acf0fff3a65da121d
K0teize <ktv110687@gmail.com>
2025-07-14 20:09:45 +0600

    feat: add dynamic library realisation

---
+++ FileSystemCommands/FileSystemCommands.cs
@@ -0,0 +1,35 @@
+﻿namespace FileSystemCommands;
+
+using System.ComponentModel;
+using CommandLib;
+
+public class DirectorySizeCommand : ICommand
+{
+    private readonly string _path;
+    public long size { get; private set; }
+    public DirectorySizeCommand(string path)
+    {
+        _path = path;
+    }
+    public void Execute()
+    {
+        DirectoryInfo directory = new DirectoryInfo(_path);
+        if (directory.Exists) size = directory.GetFiles().Sum(f => f.Length);
+    }
+}
+public class FindFilesCommand : ICommand
+{
+    private readonly string _path;
+    private readonly string _mask;
+    public List<string> files { get; private set; } = new List<string>();
+    public FindFilesCommand(string path, string mask)
+    {
+        _path = path;
+        _mask = mask;
+    }
+    public void Execute()
+    {
+        DirectoryInfo directory = new DirectoryInfo(_path);
+        if (directory.Exists) files = directory.GetFiles(_mask).Select(f=>f.Name).ToList();
+    }
+}
---
+++ task08/CommandRunner.cs
@@ -0,0 +1,41 @@
+﻿namespace task08;
+
+using System.Reflection;
+using CommandLib;
+using FileSystemCommands;
+
+public class CommandRunner
+{
+    public static void Main()
+    {
+        var assembly = Assembly.LoadFrom("FileSystemCommands.dll");
+
+        var testDir1 = Path.Combine(Path.GetTempPath(), "TestDir1");
+        Directory.CreateDirectory(testDir1);
+        File.WriteAllText(Path.Combine(testDir1, "test1.txt"), "Hello");
+        File.WriteAllText(Path.Combine(testDir1, "test2.txt"), "World");
+        Type? sizeCommandType = assembly.GetType("FileSystemCommands.DirectorySizeCommand");
+
+        if (sizeCommandType != null && Activator.CreateInstance(sizeCommandType, testDir1) is DirectorySizeCommand sizeCommand)
+        {
+            sizeCommand.Execute();
+            Console.WriteLine(sizeCommand.size);
+        }
+
+        var testDir2 = Path.Combine(Path.GetTempPath(), "TestDir2");
+        Directory.CreateDirectory(testDir2);
+        File.WriteAllText(Path.Combine(testDir2, "file1.txt"), "Text");
+        File.WriteAllText(Path.Combine(testDir2, "file2.txt"), "Log");
+        Type? fileCommandType = assembly.GetType("FileSystemCommands.FindFilesCommand");
+
+        if (fileCommandType != null && Activator.CreateInstance(fileCommandType, testDir2, "*.txt") is FindFilesCommand filesCommand)
+        {
+            filesCommand.Execute();
+            foreach (var file in filesCommand.files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+    
+}
