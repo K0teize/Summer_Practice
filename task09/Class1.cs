b12bccaad06449849f1f70a5d5a81dcb8cf4573d
K0teize <ktv110687@gmail.com>
2025-07-15 14:50:36 +0600

    feat: add metadata library realisation

---
+++ task09/Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace task09;
+
+using System.Reflection;
+using task06;
+public class MetadataAnalyze
+{
+    public static void PrintInfo(string path)
+    {
+        Assembly assembly = Assembly.LoadFrom(path);
+        Type[] types = assembly.GetTypes();
+        foreach (Type type in types)
+        {
+            Console.WriteLine(type.FullName);
+            foreach (var method in type.GetMethods())
+            {
+                Console.WriteLine(method.Name);
+                foreach (var param in method.GetParameters())
+                {
+                    Console.WriteLine($"{param.Name}, {param.ParameterType.Name}");
+                }
+            }
+            foreach (var attr in type.GetCustomAttributes())
+            {
+                Console.WriteLine(attr.GetType().Name);
+            }
+            foreach (var constr in type.GetConstructors())
+            {
+                Console.WriteLine(constr.Name);
+                foreach (var param in constr.GetParameters())
+                {
+                    Console.WriteLine($"{param.Name}, {param.ParameterType.Name}");
+                }
+            }
+        }
+    }
+}
