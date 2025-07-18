2505ba266368e82bbe3bc0a9fcecf107113a496f
K0teize <ktv110687@gmail.com>
2025-07-18 12:21:16 +0600

    feat: add integral realisation and tests

---
+++ task14/Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace task14;
+
+public class DefiniteIntegral
+{
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsnumber)
+    {
+        double total = 0.0;
+        double interval = (b - a) / threadsnumber;
+
+        int totalSteps = (int)((b - a) / step);
+        int stepsPerThread = totalSteps / threadsnumber;
+
+        Parallel.For(0, threadsnumber, () => 0.0, (i, state, localSum) =>
+        {
+            double start = a + i * interval;
+            double end = (i == threadsnumber - 1) ? b : start + interval;
+
+            double x = start;
+            double localStep = (end - start) / stepsPerThread;
+
+            for (int j = 0; j < stepsPerThread; j++)
+            {
+                localSum += (function(x) + function(x + localStep)) * 0.5 * localStep;
+                x += localStep;
+            }
+
+            return localSum;
+        },
+        localSum =>
+        {
+            Interlocked.Exchange(ref total, total + localSum);
+        });
+
+        return total;
+    }
+}
---
+++ task14tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace task14tests;
+
+using task14;
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var X = (double x) => x;
+        var SIN = (double x) => Math.Sin(x);
+
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
+
+        Assert.Equal(12.5, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 1e-5);
+    }
+}
